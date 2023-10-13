5084c102d3d41c6f2e8d87be2b145f967c56cf1c
Christian Obereder <50083960+GitianOberhuber@users.noreply.github.com>
2023-10-13 20:52:58 +0200

    added script for replacing files of the original DRS_parsing repo with the ones provided by this repo

---
+++ patch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Trying to replace original Neural_DRS files..."
+
+cd ..
+if [ ! -d "src" ]; then
+    echo "The directory src does not exist, make sure the original DRS_Parsing repository is present! Exiting..."
+    exit 1  # Exit with a non-zero status code to indicate an error
+fi
+if [ ! -d "src/allennlp_scripts" ]; then
+    echo "The directory src/allennlp_scripts does not exist, make sure the original DRS_Parsing repository is present! Exiting..."
+    exit 1  # Exit with a non-zero status code to indicate an error
+fi
+if [ ! -d "DRS_parsing" ]; then
+    echo "The directory DRS_parsing does not exist, make sure the original DRS_Parsing repository is present! Exiting..."
+    exit 1  # Exit with a non-zero status code to indicate an error
+fi
+if [ ! -d "DRS_parsing/evaluation" ]; then
+    echo "The directory DRS_parsing/evaluation does not exist, make sure the original DRS_Parsing repository is present! Exiting..."
+    exit 1  # Exit with a non-zero status code to indicate an error
+fi
+
+cp -u -R Neural_DRS_patch/src src
+cp -u -R Neural_DRS_patch/DRS_parsing DRS_parsing
+echo "Successfully replaced files!"
+
+
